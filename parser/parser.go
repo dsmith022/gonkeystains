e31aadb3741712b8542402279bef9478a4d05d90
dsmith022 <smidane022@gmail.com>
2026-06-27 11:33:42 -0500

    created ast and parser packages, began basic implementations. LetStatements and Identifer AST types implemented, defined Expression and Statement AST interfaces.

---
+++ parser/parser.go
@@ -0,0 +1,30 @@
+package parser
+
+import (
+	"github.com/dsmith022/gonkeystains.git/ast"
+	"github.com/dsmith022/gonkeystains.git/lexer"
+	"github.com/dsmith022/gonkeystains.git/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
